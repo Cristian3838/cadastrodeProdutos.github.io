5e04180dcaba679b24a6f66a6522ef489fbef3b7
Cristian3838 <cristian.ataliba@outlook.com>
2022-02-05 18:08:03 -0300

    Curso de Angular 9

---
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -0,0 +1,31 @@
+import { Product } from './../product.model';
+import { Router } from '@angular/router';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-read',
+  templateUrl: './product-read.component.html',
+  styleUrls: ['./product-read.component.css']
+})
+export class ProductReadComponent implements OnInit {
+
+  products!: Product[]
+  displayedColumns = ['id', 'name', 'price', 'action']
+
+  constructor(private productService: ProductService
+    ) { }
+
+  ngOnInit(): void {
+
+    this.productService.read().subscribe((products) =>{
+      this.products = products
+      console.log(products)
+
+    })
+  }
+
+
+  
+
+}
